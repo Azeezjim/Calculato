ee3403f71fcea1e6031fd3a4bcc24cf823964962
Azeezjim <azeexjim@gmail.com>
2022-05-18 06:40:20 -1100

    latest commit of the day

---
+++ calculator.js
@@ -0,0 +1,12 @@
+let result = document.getElementById('inputtext');
+
+let calculate= (number) => {
+  result.value = result.value + number
+}
+
+let Result = () => {
+
+  try{
+    result.value
+  }
+}
